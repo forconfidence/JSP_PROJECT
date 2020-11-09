d8520e876589b84189dd1f36afa771f3d5c2ceb9
forconfidence <73941202+forconfidence@users.noreply.github.com>
2020-11-09 13:22:51 +0900

    Add files via upload
    변수이름 변경

--- confidence_db.sql
+++ confidence_db.sql
@@ -1,25 +1,26 @@
+DROP TABLE reply;
 DROP TABLE board;
 DROP TABLE member;
-DROP TABLE reply;
+
 
 CREATE TABLE board
 (
-	board_ID             NUMBER(8) NOT NULL ,
+	boardId             NUMBER(8) NOT NULL ,
 	nickname             CHAR(18) NULL ,
-	writedate            DATE NULL ,
+	writeDate            DATE NULL ,
 	content              VARCHAR2(15) NULL ,
-	viewcount            NUMBER(8) NULL ,
+	viewCount            NUMBER(8) NULL ,
 	title                CHAR(18) NULL ,
-	member_ID            CHAR(18) NOT NULL 
+	memberId            CHAR(18) NOT NULL 
 );
 CREATE UNIQUE INDEX XPKboard ON board
-(board_ID   ASC);
+(boardId   ASC);
 ALTER TABLE board
-	ADD CONSTRAINT  XPKboard PRIMARY KEY (board_ID);
+	ADD CONSTRAINT  XPKboard PRIMARY KEY (boardId);
 
 CREATE TABLE member
 (
-	member_ID            CHAR(18) NOT NULL ,
+	memberId            CHAR(18) NOT NULL ,
 	name                 CHAR(18) NULL ,
 	password             CHAR(18) NULL ,
 	birthdate            DATE NULL ,
@@ -28,26 +29,24 @@ CREATE TABLE member
 	nickname             CHAR(18) NULL 
 );
 CREATE UNIQUE INDEX XPKmember ON member
-(member_ID   ASC);
+(memberId   ASC);
 ALTER TABLE member
-	ADD CONSTRAINT  XPKmember PRIMARY KEY (member_ID);
+	ADD CONSTRAINT  XPKmember PRIMARY KEY (memberId);
 
 CREATE TABLE reply
 (
-	reply_ID             NUMBER(8) NOT NULL ,
+	replyId             NUMBER(8) NOT NULL ,
 	content              VARCHAR2(15 CHAR) NULL ,
-	replydate            DATE NULL ,
+	replyDate            DATE NULL ,
 	nickname             CHAR(18) NULL ,
-	board_ID             NUMBER(8) NOT NULL 
+	boardId             NUMBER(8) NOT NULL 
 );
 CREATE UNIQUE INDEX XPKreply ON reply
-(reply_ID   ASC);
+(replyId   ASC);
 ALTER TABLE reply
-	ADD CONSTRAINT  XPKreply PRIMARY KEY (reply_ID);
+	ADD CONSTRAINT  XPKreply PRIMARY KEY (replyId);
 ALTER TABLE board
-	ADD (CONSTRAINT R_4 FOREIGN KEY (member_ID) REFERENCES member (member_ID));
-
-
+	ADD (CONSTRAINT R_4 FOREIGN KEY (memberId) REFERENCES member (memberId));
 
 ALTER TABLE reply
-	ADD (CONSTRAINT R_3 FOREIGN KEY (board_ID) REFERENCES board (board_ID));
+	ADD (CONSTRAINT R_3 FOREIGN KEY (boardId) REFERENCES board (boardId));
